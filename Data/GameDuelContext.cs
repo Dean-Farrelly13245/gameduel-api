0ca088968c4f3b32a14ecbdd7e8554081ade9ac0
Gvidas Vanagas <x00204010@mytudublin.ie>
2026-04-09 21:55:50 +0100

    Added GameDuelContext

---
+++ Data/GameDuelContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GameDuel.API.Models;
+
+namespace GameDuel.API.Data
+{
+    public class GameDuelContext : DbContext
+    {
+        public GameDuelContext(DbContextOptions<GameDuelContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Matchup> Matchups { get; set; }
+    }
+}
