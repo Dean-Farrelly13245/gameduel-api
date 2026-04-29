68c31e01b85a391427d051fe796a6988028c9e9b
Dean-F13245 <deanfarrelly00@gmail.com>
2026-04-29 17:30:29 +0100

    added build step - CD/CD

--- Program.cs
+++ Program.cs
@@ -8,22 +8,21 @@ namespace GameDuel.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             builder.Services.AddDbContext<GameDuelContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("GameDuelDb")));
 
             var app = builder.Build();
 
-            
             app.UseSwagger();
             app.UseSwaggerUI();
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
+
+
 
             app.Run();
         }
